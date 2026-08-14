377827ae4e18a0e27bb4f1c1ff67fd9756baacc9
Rinku <gangwarrinku205@gmail.com>
2026-08-14 10:36:39 +0530

    buy and sell stock 2nd

---
+++ Arrays/0122.Buy_and _sell_stock_2.java
@@ -0,0 +1,22 @@
+/*122. Best Time to Buy and Sell Stock II
+
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can sell and buy the stock multiple times on the same day, ensuring you never hold more than one share of the stock.
+
+Find and return the maximum profit you can achieve.*/
+
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit=0;
+        for(int i=0;i<prices.length-1;i++){
+            if(prices[i]<prices[i+1]){
+                maxProfit=maxProfit+(prices[i+1]-prices[i]);
+            }
+        }
+        return maxProfit;
+        
+    }
+}
