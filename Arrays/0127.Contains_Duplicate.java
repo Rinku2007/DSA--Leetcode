925d1c973371541764bb1bf65bf97f9e31460db4
Rinku <gangwarrinku205@gmail.com>
2026-08-14 12:49:46 +0530

    contains duplicate (by sorting)

--- Arrays/0127.Contains_Duplicate.java
+++ Arrays/0127.Contains_Duplicate.java
@@ -24,4 +24,16 @@ class Solution {
 
 
 /* By sorting  */
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
 
